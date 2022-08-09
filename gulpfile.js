73cf55feea0ec949d818c662cf9cd971e60562f8
Josh Bruce <josh@8fold.pro>
2022-08-09 15:11:49 -0500

    prepare: Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require("gulp");
 const rename = require("gulp-rename");
 
-const rawDestLocal   = "./site-raw/local";
-const rawDestPublic  = "./site-raw/public";
+// const rawDestLocal   = "./site-raw/local";
+// const rawDestPublic  = "./site-raw/public";
 const rootDestLocal  = "./site-root/local";
-const rootDestPublic = "./site-root/public";
-const loreDestLocal  = "./site-lore/local";
-const loreDestPublic = "./site-lore/public";
+// const rootDestPublic = "./site-root/public";
+// const loreDestLocal  = "./site-lore/local";
+// const loreDestPublic = "./site-lore/public";
 
 gulp.task("default", (done) => {
   console.log('gulp sass: Compiles the CSS');
@@ -36,21 +36,21 @@ gulp.task("sass", (done) => {
       sass({ outputStyle: "expanded" })
       .on("error", () => { console.log('ERROR: while compiling Sass') }))
     .pipe(postcss(pluginsProcess))
-    .pipe(gulp.dest(rawDestLocal + '/css'))
-    .pipe(gulp.dest(rawDestPublic + '/css'))
+    // .pipe(gulp.dest(rawDestLocal + '/css'))
+    // .pipe(gulp.dest(rawDestPublic + '/css'))
     .pipe(gulp.dest(rootDestLocal + '/css'))
-    .pipe(gulp.dest(rootDestPublic + '/css'))
-    .pipe(gulp.dest(loreDestLocal + '/css'))
-    .pipe(gulp.dest(loreDestPublic + '/css'))
+    // .pipe(gulp.dest(rootDestPublic + '/css'))
+    // .pipe(gulp.dest(loreDestLocal + '/css'))
+    // .pipe(gulp.dest(loreDestPublic + '/css'))
     .pipe(postcss(pluginsMinify))
     .pipe(rename({suffix: ".min"}))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(rawDestLocal + '/css'))
-    .pipe(gulp.dest(rawDestPublic + '/css'))
-    .pipe(gulp.dest(rootDestLocal + '/css'))
-    .pipe(gulp.dest(rootDestPublic + '/css'))
-    .pipe(gulp.dest(loreDestLocal + '/css'))
-    .pipe(gulp.dest(loreDestPublic + '/css'));
+    // .pipe(gulp.dest(rawDestLocal + '/css'))
+    // .pipe(gulp.dest(rawDestPublic + '/css'))
+    .pipe(gulp.dest(rootDestLocal + '/css'));
+    // .pipe(gulp.dest(rootDestPublic + '/css'))
+    // .pipe(gulp.dest(loreDestLocal + '/css'))
+    // .pipe(gulp.dest(loreDestPublic + '/css'));
   done();
 });
 
@@ -66,12 +66,12 @@ gulp.task("javascript", (done) => {
     .src(src)
     .pipe(uglify())
     .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest(rawDestLocal + '/js'))
-    .pipe(gulp.dest(rawDestPublic + '/js'))
-    .pipe(gulp.dest(rootDestLocal + '/js'))
-    .pipe(gulp.dest(rootDestPublic + '/js'))
-    .pipe(gulp.dest(loreDestLocal + '/js'))
-    .pipe(gulp.dest(loreDestPublic + '/js'));
+    // .pipe(gulp.dest(rawDestLocal + '/js'))
+    // .pipe(gulp.dest(rawDestPublic + '/js'))
+    .pipe(gulp.dest(rootDestLocal + '/js'));
+    // .pipe(gulp.dest(rootDestPublic + '/js'))
+    // .pipe(gulp.dest(loreDestLocal + '/js'))
+    // .pipe(gulp.dest(loreDestPublic + '/js'));
   done();
 });
 
@@ -83,12 +83,12 @@ gulp.task("favicons", (done) => {
 
   gulp
     .src(src + '/**/*')
-    .pipe(gulp.dest(rawDestLocal + '/favicons'))
-    .pipe(gulp.dest(rawDestPublic + '/favicons'))
-    .pipe(gulp.dest(rootDestLocal + '/favicons'))
-    .pipe(gulp.dest(rootDestPublic + '/favicons'))
-    .pipe(gulp.dest(loreDestLocal + '/favicons'))
-    .pipe(gulp.dest(loreDestPublic + '/favicons'));
+    // .pipe(gulp.dest(rawDestLocal + '/favicons'))
+    // .pipe(gulp.dest(rawDestPublic + '/favicons'))
+    .pipe(gulp.dest(rootDestLocal + '/favicons'));
+    // .pipe(gulp.dest(rootDestPublic + '/favicons'))
+    // .pipe(gulp.dest(loreDestLocal + '/favicons'))
+    // .pipe(gulp.dest(loreDestPublic + '/favicons'));
   done();
 });
 
@@ -100,12 +100,12 @@ gulp.task("ui", (done) => {
 
   gulp
     .src(src + "/**/*")
-    .pipe(gulp.dest(rawDestLocal + "/ui"))
-    .pipe(gulp.dest(rawDestPublic + '/ui'))
-    .pipe(gulp.dest(rootDestLocal + '/ui'))
-    .pipe(gulp.dest(rootDestPublic + '/ui'))
-    .pipe(gulp.dest(loreDestLocal + '/ui'))
-    .pipe(gulp.dest(loreDestPublic + '/ui'));
+    // .pipe(gulp.dest(rawDestLocal + "/ui"))
+    // .pipe(gulp.dest(rawDestPublic + '/ui'))
+    .pipe(gulp.dest(rootDestLocal + '/ui'));
+    // .pipe(gulp.dest(rootDestPublic + '/ui'))
+    // .pipe(gulp.dest(loreDestLocal + '/ui'))
+    // .pipe(gulp.dest(loreDestPublic + '/ui'));
   done();
 });
 
@@ -117,16 +117,28 @@ gulp.task("fonts", (done) => {
 
   gulp
     .src(src + "/**/*")
-    .pipe(gulp.dest(rawDestLocal + '/fonts'))
-    .pipe(gulp.dest(rawDestPublic + '/fonts'))
-    .pipe(gulp.dest(rootDestLocal + '/fonts'))
-    .pipe(gulp.dest(rootDestPublic + '/fonts'))
-    .pipe(gulp.dest(loreDestLocal + '/fonts'))
-    .pipe(gulp.dest(loreDestPublic + '/fonts'));
+    // .pipe(gulp.dest(rawDestLocal + '/fonts'))
+    // .pipe(gulp.dest(rawDestPublic + '/fonts'))
+    .pipe(gulp.dest(rootDestLocal + '/fonts'));
+    // .pipe(gulp.dest(rootDestPublic + '/fonts'))
+    // .pipe(gulp.dest(loreDestLocal + '/fonts'))
+    // .pipe(gulp.dest(loreDestPublic + '/fonts'));
   done();
 });
 
-gulp.task("watch", () => {
-  gulp.watch("./src/assets/sass/**/*.scss", gulp.series("sass"));
-  return;
+/**
+ * Begin images
+ */
+gulp.task("images", (done) => {
+  const src = "./src/assets/profiles";
+
+  gulp
+    .src(src + '/**/*')
+    // .pipe(gulp.dest(rawDestLocal + '/favicons'))
+    // .pipe(gulp.dest(rawDestPublic + '/favicons'))
+    .pipe(gulp.dest(rootDestLocal + '/profiles'));
+    // .pipe(gulp.dest(rootDestPublic + '/favicons'))
+    // .pipe(gulp.dest(loreDestLocal + '/favicons'))
+    // .pipe(gulp.dest(loreDestPublic + '/favicons'));
+  done();
 });
